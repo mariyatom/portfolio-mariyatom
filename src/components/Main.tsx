b9d99d1185ab3d494830e3881a9d2a879c512f94
mariyatom <mariyatom07@gmail.com>
2025-04-29 17:19:13 +1200

    portfolio2

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,32 +1,70 @@
-import React from "react";
-import GitHubIcon from '@mui/icons-material/GitHub';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import '../assets/styles/Main.scss';
+import DescriptionIcon from '@mui/icons-material/Description'
+import GitHubIcon from '@mui/icons-material/GitHub'
+import LinkedInIcon from '@mui/icons-material/LinkedIn'
+// import mariyaAvatar from '../assets/images/mariya.png'
+import '../assets/styles/Main.scss'
 
 function Main() {
-
   return (
     <div className="container">
       <div className="about-section">
-        <div className="image-wrapper">
-          <img src="https://my-aws-assets.s3.us-west-2.amazonaws.com/portfolio-img/avatar_circle.jpeg" alt="Avatar" />
-        </div>
+        {/* <div className="image-wrapper">
+          <img src={mariyaAvatar} alt="Avatar" />
+        </div> */}
         <div className="content">
           <div className="social_icons">
-            <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-            <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+            <a
+              href="https://github.com/mariyatom"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <GitHubIcon />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/tommariya/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <LinkedInIcon />
+            </a>
+            <a
+              href="/public/mariyatom-CV.pdf"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <DescriptionIcon />
+            </a>
           </div>
-          <h1>Yuji Sato</h1>
-          <p>Full Stack Engineer</p>
+          <h1>Mariya Tom</h1>
+          <p>Full-Stack Software Developer</p>
 
           <div className="mobile_social_icons">
-            <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-            <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+            <a
+              href="https://github.com/mariyatom"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <GitHubIcon />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/tommariya/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <LinkedInIcon />
+            </a>
+            <a
+              href="/public/mariyatom-CV.pdf"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <DescriptionIcon />
+            </a>
           </div>
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default Main;
+export default Main
